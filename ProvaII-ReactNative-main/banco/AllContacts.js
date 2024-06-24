b106c7b57b3d6e926affb758a1e1c86c68c6cdbb
GabrielCamelo9999 <135195437+GabrielCamelo9999@users.noreply.github.com>
2024-06-24 19:53:28 -0300

    Add files via upload

---
+++ ProvaII-ReactNative-main/banco/AllContacts.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import { Alert, Button, FlatList, Text, View, SafeAreaView, StyleSheet } from 'react-native';
+import { create } from './Create.js';
+
+export function AllContacts() {
+    let [flatListItems, setFlatListItems] = useState([]);
+
+    const getAll = async () => {
+        try{
+             db = await create();
+             let allRows = await db.getAllAsync('select * from senhas;');
+             setFlatListItems(allRows);
+             console.log("[LOG] Data retrieved from tables senhas");
+             if(allRows.length==0){
+                 Alert.alert(
+                     'Alerta',
+                     'Nenhuma senha salva',
+                     [
+                         {
+                             text: 'Ok'
+                         },
+                     ],
+                     { cancelable: false }
+                 );
+             }
+         } catch (error) {
+             console.log(error);
+         }
+     
+     }
+
+     let listItemView = (item) => {
+        return (
+            <View
+                key={item.id}
+                style={{ backgroundColor: '#EEE', marginTop: 20, padding: 30, borderRadius: 10 }}>
+                <Text style={styles.textheader}>Senha</Text>
+                <Text style={styles.textbottom}>id: {item.id}</Text>
+                <Text style={styles.textbottom}>senha: {item.senha}</Text>
+            </View>
+        );
+    };
+
+    return (
+        <View style={{ flex: 1, width: "80%", marginTop: 10 }}>
+            <Button title="Todas as senhas" onPress={() => getAll()} />
+            <View style={{ flex: 1, backgroundColor: 'white'}}>
+                <View style={{ flex: 1 }}>
+                    <FlatList
+                        style={{ marginTop: 30 }}
+                        contentContainerStyle={{ paddingHorizontal: 20 }}
+                        data={flatListItems}
+                        keyExtractor={(item, index) => index.toString()}
+                        renderItem={({ item }) => listItemView(item)}
+                    />
+                </View>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    textbottom: {
+        color: '#111',
+        fontSize: 18,
+    },
+});
